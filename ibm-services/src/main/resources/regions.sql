f18e64e6ce44f10b191f2c46b06beb0a06a18c96
anbazhagan natarajan <anbazhagan.natarajan1985@gmail.com>
2019-02-20 20:20:28 +0530

    regions and vendors sql

---
+++ ibm-services/src/main/resources/regions.sql
@@ -0,0 +1,15 @@
+inserts into regions ('region_code', 'region_name') value ('USA', 'USA');
+inserts into regions ('region_code', 'region_name') value ('IN-GDC', 'INDIA GDC');
+inserts into regions ('region_code', 'region_name') value ('IN-DOM', 'INDIA DOM');
+inserts into regions ('region_code', 'region_name') value ('NOR-H', 'NORDICS-Home');
+inserts into regions ('region_code', 'region_name') value ('NOR-DC', 'NORDICS-DC');
+inserts into regions ('region_code', 'region_name') value ('CZ-GDC', 'CZECH GDC');
+inserts into regions ('region_code', 'region_name') value ('ANZ', 'ANZ');
+inserts into regions ('region_code', 'region_name') value ('AFR', 'AFRICA');
+inserts into regions ('region_code', 'region_name') value ('US-SME', 'USA SME');
+
+inserts into regions ('id', 'name') value ('1', 'Vendor 1');
+inserts into regions ('id', 'name') value ('2', 'Vendor 2');
+inserts into regions ('id', 'name') value ('3', 'Vendor 3');
+inserts into regions ('id', 'name') value ('4', 'Vendor 4');
+
